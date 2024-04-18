8dd9c63cecfa923625d1a7d646f1b4b5f7fdb8e8
scottzwells <167091359+scottzwells@users.noreply.github.com>
2024-04-18 13:52:50 +0800

    Add files via upload

---
+++ 对象.js
@@ -0,0 +1,13 @@
+const person={
+    age:30,
+    address:{
+        street:"50 main st",
+        city :"Boston",
+        state:"MA"
+    }
+}
+console.log(person)
+console.log(person.address)
+let {age} =person
+age =10
+console.log(person)
